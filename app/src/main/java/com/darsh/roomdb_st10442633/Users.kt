1380b5ecedf9d3fe7fe3e2afdfee61ea249ce605
darshen-nadar <166602264+darshen-nadar@users.noreply.github.com>
2026-04-26 03:25:29 +0200

    Second Commit. User Entity added for application authentication.

---
+++ app/src/main/java/com/darsh/roomdb_st10442633/Users.kt
@@ -0,0 +1,17 @@
+package com.darsh.roomdb_st10442633
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val userId: Long = 0,
+    val username: String,
+    //val password: String, (Not used yet)
+    val email: String? = null,
+    val fullName: String? = null,
+    val createdAt: Date = Date(),
+    val lastLogin: Date? = null
+)

db0406efec3813b25aecae53706bde8d6b0a9c00
ST10382483 <ST10382483@vcconnect.edu.za>
2026-04-26 23:28:19 +0200

    12th commit
    added comments to transaction data

--- BudTrackerTransactionST10382483/app/src/main/java/TransactionData.kt
+++ BudTrackerTransactionST10382483/app/src/main/java/TransactionData.kt
@@ -1,5 +1,10 @@
 package com.jordan.budtrackertransactionst10382483
 
+// This object acts as a temporary storage (like a simple database)
+// "object" means there will only be ONE shared instance used across the whole app
 object TransactionData {
+
+    // This is a list that stores all transactions
+    // mutableListOf means we can add, remove, and change items in the list
     val transactionList = mutableListOf<Transaction>()
 }

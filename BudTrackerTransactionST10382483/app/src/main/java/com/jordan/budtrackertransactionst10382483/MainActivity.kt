5ec78512d2a84c00e56d7ec0c5c941f4de78eaae
ST10382483 <ST10382483@vcconnect.edu.za>
2026-04-26 22:50:51 +0200

    8th commit
    added comments to my main activity to better explain my work

--- BudTrackerTransactionST10382483/app/src/main/java/com/jordan/budtrackertransactionst10382483/MainActivity.kt
+++ BudTrackerTransactionST10382483/app/src/main/java/com/jordan/budtrackertransactionst10382483/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.jordan.budtrackertransactionst10382483
 
-import android.content.Intent
+import android.content.Intent // Used to move between activities (screens)
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+// MainActivity represents the Transactions screen of the app
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Connect this activity to its XML layout file
         setContentView(R.layout.activity_main)
 
+        // Link the button from XML to Kotlin using its ID
         val btnViewGraphs = findViewById<Button>(R.id.btnViewGraphs)
 
+        // Set what happens when the button is clicked
         btnViewGraphs.setOnClickListener {
-            // add here
-            startActivity(Intent(this, GraphsActivity::class.java)) // add here
+
+            // Create an Intent to move from MainActivity to GraphsActivity
+            val intent = Intent(this, GraphsActivity::class.java)
+
+            // Start the GraphsActivity (navigate to graph screen)
+            startActivity(intent) // add here
         }
     }
 }

ebaddf3d777ea802c28f69bccf867072d63db95a
darshen-nadar <166602264+darshen-nadar@users.noreply.github.com>
2026-04-26 03:54:57 +0200

    Fourth Commit. Expense entity added. Category.kt changed to a normal Kotlin file.

---
+++ app/src/main/java/com/darsh/roomdb_st10442633/Expenses.kt
@@ -0,0 +1,45 @@
+package com.darsh.roomdb_st10442633
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.util.Date
+import java.util.Locale.Category
+
+@Entity(
+    tableName = "expense_entries",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = ["categoryId"],
+            childColumns = ["categoryId"]
+            
+        )
+    ],
+    indices = [Index("userId"), Index("categoryId"), Index("date")]
+)
+data class ExpenseEntry(
+    @PrimaryKey(autoGenerate = true)
+    val expenseId: Long = 0,
+    val userId: Long,
+    val categoryId: Long,
+    val date: Date,
+    val startTime: Date,
+    val endTime: Date,
+    val description: String,
+    val amount: Double,
+    val photoUri: String? = null,
+    val photoPath: String? = null,
+    val notes: String? = null,
+    val location: String? = null,
+    val isSynced: Boolean = false,
+    val createdAt: Long = System.currentTimeMillis(),
+    val updatedAt: Long = System.currentTimeMillis()
+)

4f4e301132306c39125fb2018ba5a52cdcaad762
St10441232 <st10441232@users.noreply.github.com>
2026-04-27 15:08:24 +0200

    COMMIT 8
    Added goal type spinner options as well as passed created goal data to goals screen

--- app/src/main/java/com/aiden/budtrackr_st10441232/CreateGoalActivity.kt
+++ app/src/main/java/com/aiden/budtrackr_st10441232/CreateGoalActivity.kt
@@ -1,9 +1,12 @@
 package com.aiden.budtrackr_st10441232
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 
 class CreateGoalActivity : Activity() {
@@ -13,9 +16,21 @@ class CreateGoalActivity : Activity() {
         setContentView(R.layout.activity_create_goal)
 
         val name = findViewById<EditText>(R.id.goalName)
+        val typeSpinner = findViewById<Spinner>(R.id.goalTypeSpinner)
         val target = findViewById<EditText>(R.id.goalTarget)
         val current = findViewById<EditText>(R.id.goalCurrent)
         val saveBtn = findViewById<Button>(R.id.saveGoalBtn)
+
+        val goalTypes = arrayOf("Special Goal", "Smart Goal", "Consumer Goal")
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            goalTypes
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        typeSpinner.adapter = adapter
 
         saveBtn.setOnClickListener {
             if (
@@ -24,6 +39,13 @@ class CreateGoalActivity : Activity() {
                 current.text.toString().isEmpty()
             ) {
                 Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, GoalsActivity::class.java)
+                intent.putExtra("name", name.text.toString())
+                intent.putExtra("type", typeSpinner.selectedItem.toString())
+                intent.putExtra("target", target.text.toString())
+                intent.putExtra("current", current.text.toString())
+                startActivity(intent)
             }
         }
     }

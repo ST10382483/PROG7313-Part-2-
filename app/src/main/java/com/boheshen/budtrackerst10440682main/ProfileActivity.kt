345124dcc882fda672e23d1682d2a542ac965336
Boheshen-Naidoo <st10440682@vcconnect.edu.za>
2026-06-01 20:43:48 +0200

    Profile .kt update
    Continued to add functionality to the main profile page.

--- app/src/main/java/com/boheshen/budtrackerst10440682main/ProfileActivity.kt
+++ app/src/main/java/com/boheshen/budtrackerst10440682main/ProfileActivity.kt
@@ -14,3 +14,26 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var txtEmail: TextView
     private lateinit var txtCellNumber: TextView
     private lateinit var btnEditDetails: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        imgProfile = findViewById(R.id.imgProfile)
+        txtFullName = findViewById(R.id.txtFullName)
+        txtNationality = findViewById(R.id.txtNationality)
+        txtEmail = findViewById(R.id.txtEmail)
+        txtCellNumber = findViewById(R.id.txtCellNumber)
+        btnEditDetails = findViewById(R.id.btnEditDetails)
+
+        loadProfileDetails()
+
+        btnEditDetails.setOnClickListener {
+            startActivity(Intent(this, EditProfileActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadProfileDetails()
+    }
